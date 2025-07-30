756edab5e7ef2a7ddf97802bb5964dffbaba5335
nik26788 <risinnshow@gmail.com>
2025-07-30 17:05:26 +0800

    feat: ✨  lint update

--- eslint.config.js
+++ eslint.config.js
@@ -5,36 +5,38 @@ import js from '@eslint/js'
 import vueEslintParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
 export default [
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                defineProps: 'readonly',
-                defineEmits: 'readonly'
-            }
-        }
-    },
-    {
-        files: ['**/*.{ts,tsx,vue}'],
-        rules: {
-            ...js.configs.recommended.rules,
-            ...pluginVue.configs['vue3-strongly-recommended'].rules,
-            'prefer-const': 'error',
-            'no-console': 'error',
-            'vue/valid-define-emits': 'error',
-            'simple-import-sort/imports': 'error',
-            'simple-import-sort/exports': 'error'
-        },
-        languageOptions: {
-            parser: vueEslintParser,
-            parserOptions: {
-                extraFileExtensions: ['.vue'],
-                parser: tsParser
-            }
-        },
-        plugins: {
-            vue: pluginVue,
-            'simple-import-sort': simpleImportSort
-        }
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        defineProps: 'readonly',
+        defineEmits: 'readonly'
+      }
     }
+  },
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginVue.configs['vue3-strongly-recommended'].rules,
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/html-self-closing': 'off',
+      'prefer-const': 'error',
+      'no-console': 'error',
+      'vue/valid-define-emits': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    },
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        extraFileExtensions: ['.vue'],
+        parser: tsParser
+      }
+    },
+    plugins: {
+      vue: pluginVue,
+      'simple-import-sort': simpleImportSort
+    }
+  }
 ]
